a371eedc602b614eec41198d19992b1e7a374267
Eric Kim-Butler <eric@kimbutler.xyz>
2023-09-10 11:49:07 +0200

    Update Deps

--- default.nix
+++ default.nix
@@ -6,17 +6,12 @@
   nodejs = nodejs-slim;
   pnpm = nodePackages.pnpm;
 
-  # We install in place to avoid a permission issue with vite-plugin-inspect copying
-  # files with readonly permissions from the nix store.
-  # It also allows us to to just copy node_modules into $out
-  installInPlace = true;
-
   src = gitignoreSource ./.;
 
   installPhase = ''
     mkdir -p $out/bin
 
-    cp -r node_modules package.json $out
+    cp -r package.json $out
     cp -r dist/public $out/bin
 
     echo "#!${nodejs}/bin/node" > $out/bin/kbf.js
