d45146c349fdbd66e3ee70d7682933f1ea12574f
Eric Kim-Butler <eric@kimbutler.xyz>
2025-01-12 16:06:26 -0500

    Fix Dev Server Load

--- app.config.ts
+++ app.config.ts
@@ -3,5 +3,10 @@ import { defineConfig } from "@solidjs/start/config";
 export default defineConfig({
   vite: {
     envPrefix: "PUBLIC_",
+    server: {
+      watch: {
+        ignored: ["**/.direnv/**", "**/.devenv/**"],
+      },
+    },
   },
 });
