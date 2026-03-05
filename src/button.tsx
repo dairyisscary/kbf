4c0f6aa8aea361ed96667ea180905bb3f2a07fbc
Eric Kim-Butler <eric@kimbutler.xyz>
2026-03-04 23:00:17 -0500

    Remove Extraneous Button `variant` HTML Attribute

--- src/button.tsx
+++ src/button.tsx
@@ -1,20 +1,21 @@
-import type { ComponentProps } from "solid-js";
+import { splitProps, type ComponentProps } from "solid-js";
 
 import clx from "~/clx";
 
 type Props = ComponentProps<"button"> & { variant?: "action" | "cancel" };
 
 export default function Button(props: Props) {
+  const [local, rest] = splitProps(props, ["class", "variant"]);
   return (
     <button
       type="button"
-      {...props}
+      {...rest}
       class={clx(
         "inline-flex items-center justify-center gap-2 border-2 border-kbf-action transition-colors duration-300 hover:bg-kbf-action-alt",
-        props.variant === "cancel"
+        local.variant === "cancel"
           ? "bg-kbf-dark-purple text-kbf-text-main aria-pressed:bg-kbf-action aria-pressed:text-kbf-text-highlight"
           : "bg-kbf-action text-kbf-text-highlight",
-        props.class,
+        local.class,
       )}
     />
   );
